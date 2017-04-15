acc07eb5e8846c3a13750f4c4fb93389c2d9b281
Samuel Venzi <samuel.venzi@me.com>
2017-04-15 18:02:45 -0300

    Incremental changer to the structure

--- Trabalho 1/agenda.asm
+++ Trabalho 1/agenda.asm
@@ -1,34 +1,42 @@
 .data
 
-menu_string: .asciiz "\nSelecione uma opção:\n1. Buscar contato\n2. Criar contato\n3. Sair\n\nOpção:"
-
+menu_string: .asciiz "\nSelecione uma opção:\n1. Visualizar agenda\n2. Buscar contato\n3. Criar contato\n4. Sair\n\nOpção:"
+invalid_msg: .asciiz "\n\nOpção inválida. Digite uma opção do menu!\n"
 .text 
 main:
-jal menu
+	jal open_file
+	jal menu
 
 
-
+	li $v0, 10
+	syscall
 menu:
 	li $v0, 4
 	la $a0, menu_string
 	syscall   # prints menu
 	
-	li   $v0, 12       
+	li $v0, 12       
  	syscall	  # gets option
  	
  	slti $t0, $v0, 0x31
- 	slti $t1, $v0, 0x34
-	xor $t3, $t1, $t0
+ 	slti $t1, $v0, 0x35
+	xor $t2, $t1, $t0
 	
-	bne $t1, 1, menu
+	beq $t2, 1, continue
+	li $v0, 4
+	la $a0, invalid_msg
+	syscall   # prints message
+	j menu
+	continue:
+	
 
 open_file:
 
-
-
+	jr $ra
+	
 close_file:
 
-
+	jr $ra
 
 create:
 
